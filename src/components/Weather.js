eddb1e2b29bd93972711770612d52273cff8f0a4
Yena <yenapaek@gmail.com>
2022-05-05 14:44:56 -0400

    manually add forecast

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,46 +3,59 @@ import React from 'react';
 function Weather({weatherData}) {
     const currentDate = new Date().toLocaleString('default', {weekday: 'short'});
     
-    const getWeatherIcon = () => {
-        const iconCode = weatherData.daily[0].weather[0].icon;
+    const getWeatherIcon = (day) => {
+        const iconCode = weatherData.daily[day].weather[0].icon;
     
         return(
             <img
                 src={`https://openweathermap.org/img/wn/${iconCode}.png`}
-                alt={weatherData.daily[0].weather[0].description}
+                alt={weatherData.daily[day].weather[0].description}
             />
         );
     }
 
     return (
         <div className="weather">
-            <div className="forcastBlock">
+            <div className="forcastBlock currentDay">
                 <div className="day">{currentDate}</div>
-                <div>{getWeatherIcon()}</div>
+                <div>{getWeatherIcon(0)}</div>
                 <div className="temp">
                     <div className="min">{weatherData.daily[0].temp.min} °</div>
                     <div className="max">{weatherData.daily[0].temp.max} °</div>     
-                </div>
-                               
+                </div>             
             </div>
-
-
-            {/* <div>{weatherData.daily[1].temp.min}</div>
-            <div>{weatherData.daily[1].temp.max}</div>
-            <div>{weatherData.daily[1].weather[0].main}</div>
-
-            <div>{weatherData.daily[2].temp.min}</div>
-            <div>{weatherData.daily[2].temp.max}</div>            
-            <div>{weatherData.daily[2].weather[0].main}</div>
-
-            <div>{weatherData.daily[3].temp.min}</div>
-            <div>{weatherData.daily[3].temp.max}</div>
-            <div>{weatherData.daily[3].weather[0].main}</div>
-
-            <div>{weatherData.daily[4].temp.min}</div>
-            <div>{weatherData.daily[4].temp.max}</div>
-            <div>{weatherData.daily[4].weather[0].main}</div> */}
-            
+            <div className="forcastBlock">
+                <div className="day">{currentDate}</div>
+                <div>{getWeatherIcon(1)}</div>
+                <div className="temp">
+                    <div className="min">{weatherData.daily[1].temp.min} °</div>
+                    <div className="max">{weatherData.daily[1].temp.max} °</div>     
+                </div>             
+            </div>
+            <div className="forcastBlock">
+                <div className="day">{currentDate}</div>
+                <div>{getWeatherIcon(2)}</div>
+                <div className="temp">
+                    <div className="min">{weatherData.daily[2].temp.min} °</div>
+                    <div className="max">{weatherData.daily[2].temp.max} °</div>     
+                </div>             
+            </div>
+            <div className="forcastBlock">
+                <div className="day">{currentDate}</div>
+                <div>{getWeatherIcon(3)}</div>
+                <div className="temp">
+                    <div className="min">{weatherData.daily[3].temp.min} °</div>
+                    <div className="max">{weatherData.daily[3].temp.max} °</div>     
+                </div>             
+            </div>
+            <div className="forcastBlock">
+                <div className="day">{currentDate}</div>
+                <div>{getWeatherIcon(4)}</div>
+                <div className="temp">
+                    <div className="min">{weatherData.daily[4].temp.min} °</div>
+                    <div className="max">{weatherData.daily[4].temp.max} °</div>     
+                </div>             
+            </div>
         </div>
     );
 }
